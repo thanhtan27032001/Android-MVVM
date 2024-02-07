ead3e02f7e2e62ec0621c7afc5700a05ec374492
ThanhTan <thanhtan27032001@gmail.com>
2024-02-07 22:25:42 +0700

    [v1.0.0] Login - bind data

--- app/src/main/java/com/example/androidmvvm/MainActivity.kt
+++ app/src/main/java/com/example/androidmvvm/MainActivity.kt
@@ -7,11 +7,15 @@ import com.example.androidmvvm.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var mainViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+        mainViewModel = MainViewModel(User("abc", "def"))
+        binding.mainViewModel = mainViewModel
+
         setContentView(binding.root)
     }
 }
---
+++ app/src/main/java/com/example/androidmvvm/MainViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.androidmvvm
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel(
+    var user: User
+): ViewModel() {
+    fun login(){
+
+    }
+}
