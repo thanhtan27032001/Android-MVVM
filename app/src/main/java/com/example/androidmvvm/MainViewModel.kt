36b728cd2a4a331fbef4dc42a3661a655764f899
ThanhTan <thanhtan27032001@gmail.com>
2024-02-10 21:43:49 +0700

    [v1.0.0] Login - implement MVVM

--- app/src/main/java/com/example/androidmvvm/MainViewModel.kt
+++ app/src/main/java/com/example/androidmvvm/MainViewModel.kt
@@ -1,11 +1,19 @@
 package com.example.androidmvvm
 
+import androidx.databinding.BaseObservable
+import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
 
 class MainViewModel(
     var user: User
-): ViewModel() {
-    fun login(){
-
+): BaseObservable() {
+    val messageLogin: ObservableField<String> = ObservableField()
+    fun onClickLogin(){
+        if (user.isEmailValid() && user.isPasswordValid()) {
+            messageLogin.set("Login success")
+        }
+        else {
+            messageLogin.set("Email or Password invalid")
+        }
     }
 }
--- app/src/main/java/com/example/androidmvvm/User.kt
+++ app/src/main/java/com/example/androidmvvm/User.kt
@@ -1,8 +1,16 @@
 package com.example.androidmvvm
 
+import android.util.Patterns
+
 class User(
     var email: String,
     var password: String
 ) {
+    fun isEmailValid(): Boolean{
+        return email.isNotEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
 
+    fun isPasswordValid(): Boolean{
+        return password.length >= 6
+    }
 }
